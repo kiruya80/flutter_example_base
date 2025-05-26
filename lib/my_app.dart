82b7c4028930cccb5311ae6a5cd2a8d131cd531c
kilho <kiruya80@gmail.com>
2025-05-26 17:58:16 +0900

    2025.5.26 go router 예제

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -50,13 +50,23 @@ class ScaffoldWithNavBar extends StatelessWidget {
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: shell.currentIndex,
         onTap: (index) {
-          // QcLog.d(
-          //   'state before ===== ${GoRouterState.of(context).topRoute.toString()} , ${GoRouterState.of(context).uri} , ${shell.currentIndex} ',
-          // );
+          QcLog.d(
+            'state before ===== ${GoRouterState.of(context).topRoute.toString()} , ${GoRouterState.of(context).uri} , ${shell.currentIndex} ',
+          );
 
-          TabChangeObserver.onTabChanged(index);
-          shell.goBranch(index);
-
+          // if (index == shell.currentIndex) {
+          if (TabChangeObserver.onTabChanged(index)) {
+            /// todo 만약 홈탭으로 돌아오고 리빌드 하고 싶을때는 프로바이더나 이벤트 버스등 명시적 호출 필요
+            /// if (index == 0) {
+            ///   eventBus.fire(HomeTabSelectedEvent());
+            ///   homeTabNotifier.refresh();
+            /// }
+            ///
+            /// 동일한 탭 다시 클릭하는 경우 홈으로 이동하게
+            shell.goBranch(index, initialLocation: true);
+          } else {
+            shell.goBranch(index);
+          }
         },
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
@@ -71,10 +81,13 @@ class ScaffoldWithNavBar extends StatelessWidget {
 class TabChangeObserver {
   static int _lastIndex = 0;
 
-  static void onTabChanged(int newIndex) {
+  static bool onTabChanged(int newIndex) {
     if (_lastIndex != newIndex) {
       debugPrint('🟢 탭 변경: $_lastIndex -> $newIndex');
       _lastIndex = newIndex;
+      return false;
+    } else {
+      return true;
     }
   }
 }
