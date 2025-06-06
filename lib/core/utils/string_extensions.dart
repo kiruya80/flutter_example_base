d024172fbbd7148dffbd22c99383834bb5e267d9
kilho <kiruya80@gmail.com>
2025-06-07 03:44:41 +0900

    2025. 6. 7 StringHelper

--- lib/core/utils/string_extensions.dart
+++ lib/core/utils/string_extensions.dart
@@ -1,1 +1,23 @@
+///
+/// String extension
+///
+extension StringHelper on String? {
 
+  ///
+  /// null이 아니고 비어있지 않으면 true
+  ///
+  /// test;               False
+  /// test = "";          False
+  /// test = "flutter";   True
+  ///
+  bool get isNotNullOrEmpty => this?.isNotEmpty == true;
+
+  ///
+  /// null이거나 비어있으면 true
+  ///
+  /// test;               True
+  /// test = "";          True
+  /// test = "flutter";   False
+  ///
+  // bool get isNullOrEmpty => this?.isEmpty != false;
+}
